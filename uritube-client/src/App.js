0a96b5cc04c350d482659368886d1fbdb2d92e63
sitaruta <upatisariputa@gmail.com>
2019-09-05 10:41:49 +0900

    folder refactoring

--- uritube-client/src/App.js
+++ uritube-client/src/App.js
@@ -1,29 +1,12 @@
 import React, { Component } from 'react';
-import { fetchData } from './util/fetchData';
-import ReactHeader from './components/ReactHeader';
-
+import Index from './components/routes/Index';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    // this.eventHandler = this.eventHandler.bind(this);
-
-    this.state = {
-      videos: []
-    };
-  }
-
-  componentDidMount() {
-    fetchData({ max: 10, query: 'bts' }, data => {
-      this.setState({ videos: data.items });
-    });
-  }
-
   render() {
     return (
       <div>
-        <ReactHeader appVideos={this.state.videos} />
+        <Index />
       </div>
     );
   }
