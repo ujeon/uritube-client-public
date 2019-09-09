d5929c611422cede90a5a7429fe58381cc8e18b1
leeeunji <ej.lee005@gmail.com>
2019-09-09 10:43:49 +0900

    data, css handle in Main, HeaderForm, SiderForm

--- uritube-client/src/components/pages/Community/HederForm.js
+++ uritube-client/src/components/pages/Community/HederForm.js
@@ -3,19 +3,45 @@ import React from "react";
 import { Layout, Menu } from "antd";
 const { Header } = Layout;
 
-const HeaderForm = () => {
+const HeaderForm = props => {
   return (
     <Header className="header">
-      <div className="logo" />
+      {console.log(props)}
       <Menu
         theme="dark"
         mode="horizontal"
         defaultSelectedKeys={["2"]}
         style={{ lineHeight: "64px" }}
       >
-        <Menu.Item key="0">Uritube</Menu.Item>
-        <Menu.Item key="1">title</Menu.Item>
-        <Menu.Item key="2">nav 1</Menu.Item>
+        <Menu.Item
+          key="0"
+          style={{
+            display: "static",
+            alignContent: "space-between"
+          }}
+        >
+          Uritube
+        </Menu.Item>
+        <Menu.Item
+          key="1"
+          style={{
+            display: "static",
+            justifyContent: "space-between",
+            alignSelf: "center"
+          }}
+        >
+          title
+        </Menu.Item>
+        <Menu.Item
+          key="2"
+          style={{
+            display: "static",
+            alignContent: "space-between",
+            float: "right"
+          }}
+        >
+          Login
+        </Menu.Item>
       </Menu>
     </Header>
   );
