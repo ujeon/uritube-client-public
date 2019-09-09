435148ee218d52fc4e591591c5812a40acb6b53a
leeeunji <ej.lee005@gmail.com>
2019-09-09 11:18:34 +0900

    change for Merge

--- uritube-client/src/components/pages/Community/HederForm.js
+++ uritube-client/src/components/pages/Community/HederForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import { Link, Route, Switch } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { Layout, Menu } from "antd";
 const { Header } = Layout;
 
@@ -15,10 +15,37 @@ const HeaderForm = () => {
       >
         <Menu.Item key="0">Uritube</Menu.Item>
         <Menu.Item key="1">title</Menu.Item>
-        <Menu.Item key="2">nav 1</Menu.Item>
+        <Menu.Item key="2">
+          <Link to="/community/login">Login </Link>
+        </Menu.Item>
       </Menu>
     </Header>
   );
 };
 
 export default HeaderForm;
+
+// import React from "react";
+// // import { Link, Route, Switch } from 'react-router-dom';
+// import { Layout, Menu } from "antd";
+// const { Header } = Layout;
+
+// const HeaderForm = () => {
+//   return (
+//     <Header className="header">
+//       <div className="logo" />
+//       <Menu
+//         theme="dark"
+//         mode="horizontal"
+//         defaultSelectedKeys={["2"]}
+//         style={{ lineHeight: "64px" }}
+//       >
+//         <Menu.Item key="0">Uritube</Menu.Item>
+//         <Menu.Item key="1">title</Menu.Item>
+//         <Menu.Item key="2">nav 1</Menu.Item>
+//       </Menu>
+//     </Header>
+//   );
+// };
+
+// export default HeaderForm;
