67a70e0c6d4e6f34c3de147bdb104531d90eeac4
leeeunji <ej.lee005@gmail.com>
2019-09-09 11:29:17 +0900

    main, headerFormUser, headerForm change for Merge

--- uritube-client/src/components/pages/Community/HederForm.js
+++ uritube-client/src/components/pages/Community/HederForm.js
@@ -3,44 +3,20 @@ import { Link } from "react-router-dom";
 import { Layout, Menu } from "antd";
 const { Header } = Layout;
 
-const HeaderForm = props => {
+const HeaderForm = () => {
   return (
     <Header className="header">
-      {console.log(props)}
+      <div className="logo" />
       <Menu
         theme="dark"
         mode="horizontal"
         defaultSelectedKeys={["2"]}
         style={{ lineHeight: "64px" }}
       >
-        <Menu.Item
-          key="0"
-          style={{
-            display: "static",
-            alignContent: "space-between"
-          }}
-        >
-          Uritube
-        </Menu.Item>
-        <Menu.Item
-          key="1"
-          style={{
-            display: "static",
-            justifyContent: "space-between",
-            alignSelf: "center"
-          }}
-        >
-          title
-        </Menu.Item>
-        <Menu.Item
-          key="2"
-          style={{
-            display: "static",
-            alignContent: "space-between",
-            float: "right"
-          }}
-        >
-          Login
+        <Menu.Item key="0">Uritube</Menu.Item>
+        <Menu.Item key="1">title</Menu.Item>
+        <Menu.Item key="2">
+          <Link to="/community/login">Login </Link>
         </Menu.Item>
       </Menu>
     </Header>
@@ -50,23 +26,49 @@ const HeaderForm = props => {
 export default HeaderForm;
 
 // import React from "react";
-// // import { Link, Route, Switch } from 'react-router-dom';
+// import { Link } from "react-router-dom";
 // import { Layout, Menu } from "antd";
 // const { Header } = Layout;
 
-// const HeaderForm = () => {
+// const HeaderForm = props => {
 //   return (
 //     <Header className="header">
-//       <div className="logo" />
+//       {console.log(props)}
 //       <Menu
 //         theme="dark"
 //         mode="horizontal"
 //         defaultSelectedKeys={["2"]}
 //         style={{ lineHeight: "64px" }}
 //       >
-//         <Menu.Item key="0">Uritube</Menu.Item>
-//         <Menu.Item key="1">title</Menu.Item>
-//         <Menu.Item key="2">nav 1</Menu.Item>
+//         <Menu.Item
+//           key="0"
+//           style={{
+//             display: "static",
+//             alignContent: "space-between"
+//           }}
+//         >
+//           Uritube
+//         </Menu.Item>
+//         <Menu.Item
+//           key="1"
+//           style={{
+//             display: "static",
+//             justifyContent: "space-between",
+//             alignSelf: "center"
+//           }}
+//         >
+//           title
+//         </Menu.Item>
+//         <Menu.Item
+//           key="2"
+//           style={{
+//             display: "static",
+//             alignContent: "space-between",
+//             float: "right"
+//           }}
+//         >
+//           Login
+//         </Menu.Item>
 //       </Menu>
 //     </Header>
 //   );
--- uritube-client/src/components/pages/Main.js
+++ uritube-client/src/components/pages/Main.js
@@ -1,26 +1,13 @@
 import React, { Component } from "react";
 import MainList from "./MainList";
-import HeaderForm from "./Community/HederForm";
 import { Row } from "antd";
 import HeaderForm from "../pages/Community/HeaderForm";
 // import HeaderFormUser from '../pages/Community/HeaderFormUser';
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
-      title: "main"
-      //title 에 따른 ContentForm 렌더가 되어야 하는데.....
-    };
-    // this.titleHandling = this.titleHandling.bind(this);
-  }
-  // onClick = { this.titleHandling };
-  // 온클릭 이벤트를 어디에 걸어야 할까
-
-  // titleHandling() {
-  //   this.setState({ title: "title!!" });
-  // }
+  state = {
+    data: []
+  };
 
   async componentDidMount() {
     const data = await (await fetch(
@@ -33,26 +20,16 @@ class Main extends Component {
 
   render() {
     const { data } = this.state;
-
     return (
-      <div style={{ background: "#ECECEC", padding: "30px" }}>
-        <HeaderForm title={this.state.title} />
-        <Row gutter={16}>
-          {data.map(data => (
-            <MainList
-              key={data.id}
-              title={data.name}
-              contentsList={data.categories.map(el => (
-                <p>{el.name}</p>
-              ))}
-            />
-          ))}
-        </Row>
-        {/* <Row gutter={16}>
-          {data.map(data => (
-            <MainList key={data.id} head={data.id} title={data.title} />
-          ))}
-        </Row> */}
+      <div>
+        <HeaderForm />
+        <div style={{ background: "#ECECEC", padding: "30px" }}>
+          <Row gutter={16}>
+            {data.map(data => (
+              <MainList data={data} />
+            ))}
+          </Row>
+        </div>
       </div>
     );
   }
@@ -60,14 +37,28 @@ class Main extends Component {
 
 export default Main;
 // import React, { Component } from "react";
-// // import axios from "axios";
 // import MainList from "./MainList";
+// import HeaderForm from "./Community/HederForm";
 // import { Row } from "antd";
+// import HeaderForm from "../pages/Community/HeaderForm";
+// // import HeaderFormUser from '../pages/Community/HeaderFormUser';
 
 // class Main extends Component {
-//   state = {
-//     data: []
-//   };
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       data: [],
+//       title: "main"
+//       //title 에 따른 ContentForm 렌더가 되어야 하는데.....
+//     };
+//     // this.titleHandling = this.titleHandling.bind(this);
+//   }
+//   // onClick = { this.titleHandling };
+//   // 온클릭 이벤트를 어디에 걸어야 할까
+
+//   // titleHandling() {
+//   //   this.setState({ title: "title!!" });
+//   // }
 
 //   async componentDidMount() {
 //     const data = await (await fetch(
@@ -78,48 +69,31 @@ export default Main;
 //     });
 //   }
 
-//   // 카테고리 데이터 받아오기
-//   // getCategories = async () => {
-//   //   const data = await axios.get("http://13.125.149.171:8080/titles");
-//   //   this.setState({ data });
-//   // };
-
-//   //  componentDidMount() {
-//   //   this.getCategories();
-//   // }
-
 //   render() {
 //     const { data } = this.state;
+
 //     return (
 //       <div style={{ background: "#ECECEC", padding: "30px" }}>
+//         <HeaderForm title={this.state.title} />
 //         <Row gutter={16}>
 //           {data.map(data => (
-//             <MainList data={data} />
+//             <MainList
+//               key={data.id}
+//               title={data.name}
+//               contentsList={data.categories.map(el => (
+//                 <p>{el.name}</p>
+//               ))}
+//             />
 //           ))}
 //         </Row>
+//         {/* <Row gutter={16}>
+//           {data.map(data => (
+//             <MainList key={data.id} head={data.id} title={data.title} />
+//           ))}
+//         </Row> */}
 //       </div>
 //     );
 //   }
 // }
 
 // export default Main;
-
-// // import React, { Component } from 'react';
-// // import MainList from './MainList';
-
-// // class Main extends Component {
-
-// //   render() {
-// //     return (
-// //       <div>
-// //         {/* 헤더가 있어야 함 */}
-// //         {/* 나중에 맵으로 구현할 부분이라 list와 분리 함 */}
-// //         <MainList />
-// //         <MainList />
-// //         <MainList />
-// //       </div>
-// //     );
-// //   }
-// // }
-
-// // export default Main;
